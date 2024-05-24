a4efcc78a006e4f9a60651e14aa4f326268b93fb
tarunmalik330 <maliktarun2005@gmail.com>
2024-05-24 15:50:51 +0530

    take pull helper

--- src/components/custom-ui/DashboardCards.jsx
+++ src/components/custom-ui/DashboardCards.jsx
@@ -1,4 +1,4 @@
-export default function CustomCards(props) {
+export default function DashboardCards(props) {
   return (
     <div className="py-6 pl-6 rounded-3xl bg-white max-w-[341px] w-full max-h-[164px] h-full">
       {props.dashboardcard}
--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,10 +1,10 @@
-import CustomCards from "./custom-ui/CustomCards";
+import DashboardCards from "../custom-ui/DashboardCards";
 
 export default function Dashboard() {
   return (
     <div className="bg-mist-gray">
       <div className={"dashboard_content_width mx-auto pt-[59.54px] px-12"}>
-        <CustomCards dashboardcard="" />
+        <DashboardCards dashboardcard="" />
       </div>
     </div>
   );
