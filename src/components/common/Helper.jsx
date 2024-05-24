781c463610fed2e0b8205625dfef5657076a9acd
SunilVerma0358 <sunilverma93060@gmail.com>
2024-05-24 14:21:05 +0530

    take pull in helper

---
+++ src/components/common/Helper.jsx
@@ -0,0 +1,28 @@
+import { DashboardIcon } from "./Icon";
+
+export const SidebarTab = [
+  {
+    icon: <DashboardIcon />,
+    title: "Dashboard",
+  },
+  {
+    icon: <DashboardIcon />,
+    title: "Dashboard",
+  },
+  {
+    icon: <DashboardIcon />,
+    title: "Dashboard",
+  },
+  {
+    icon: <DashboardIcon />,
+    title: "Dashboard",
+  },
+  {
+    icon: <DashboardIcon />,
+    title: "Dashboard",
+  },
+  {
+    icon: <DashboardIcon />,
+    title: "Dashboard",
+  },
+];
