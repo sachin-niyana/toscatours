09a425d6be03849f098c4d7e2a1f066f9b84d0f4
tarunmalik330 <maliktarun2005@gmail.com>
2024-05-24 12:40:33 +0530

    create-custom-card

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,3 +1,9 @@
+import CustomCards from "./custom-ui/CustomCards";
+
 export default function Dashboard() {
-  return <div></div>;
+  return (
+    <div className="bg-light-gray">
+      <CustomCards />
+    </div>
+  );
 }
--- src/components/custom-ui/CustomCards.jsx
+++ src/components/custom-ui/CustomCards.jsx
@@ -1,5 +1,7 @@
-export default function CustomCards() {
+export default function CustomCards(props) {
   return (
-    <div className="py-6 pr-6 rounded-3xl bg-white max-w-[341px] w-full max-h-[164px] h-full"></div>
+      <div className="py-6 pr-6 rounded-3xl bg-white max-w-[341px] w-full max-h-[164px] h-full">
+          {props.dashboardcard}
+    </div>
   );
 }
