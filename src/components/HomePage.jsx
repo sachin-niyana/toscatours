fa7a458c30a942033857eee8cef26024851c029f
SunilVerma0358 <sunilverma93060@gmail.com>
2024-05-25 18:00:22 +0530

    responsive sidebar and navbar  take pull in helper

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div className="text-xl">HomePage</div>;
+  return <div>HomePage</div>;
 };
 
 export default HomePage;
--- src/components/common/Helper.jsx
+++ src/components/common/Helper.jsx
@@ -1,45 +1,54 @@
 import {
   AngeboteIcon,
-  BookingsIcon,
   BuchungenIcon,
-  CustomersIcon,
   DashboardIcon,
   KundenIcon,
-  OffersIcon,
+  LogOutIcon,
+  MeinProfileIcon,
   ProfileIcon,
+  SettingIcon,
 } from "./Icon";
 
 export const SidebarTab = [
   {
     icon: <DashboardIcon />,
     title: "Dashboard",
+    titleSmallscreen: "Dashboard",
   },
   {
     icon: <KundenIcon />,
     title: "Kunden",
+    titleSmallscreen: "Customers",
   },
   {
     icon: <AngeboteIcon />,
     title: "Angebote",
+    titleSmallscreen: "Offers",
   },
   {
     icon: <BuchungenIcon />,
     title: "Buchungen",
+    titleSmallscreen: "Bookings",
   },
-  {
-    icon: <CustomersIcon />,
-    title: "Customers",
-  },
-  {
-    icon: <OffersIcon />,
-    title: "Offers",
-  },
-  {
-    icon: <BookingsIcon />,
-    title: "Bookings",
-  },
+
   {
     icon: <ProfileIcon />,
     title: "Profile",
+    titleSmallscreen: "Profile",
+  },
+];
+
+export const SidebarDropdown = [
+  {
+    icon: <MeinProfileIcon />,
+    title: "Mein Profile",
+  },
+  {
+    icon: <SettingIcon />,
+    title: "Einstellungen",
+  },
+  {
+    icon: <LogOutIcon />,
+    title: "Logout",
   },
 ];
