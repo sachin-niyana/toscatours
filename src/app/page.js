c0d2f2349e9db1c687ebdcbcb6177929d753349d
sachin-niyana <sachinjakhar100@gmail.com>
2024-05-28 17:32:16 +0530

    add pathname

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,9 @@
-import Header from "@/components/common/Header";
-import SideBar from "@/components/common/Sidebar";
-import Customers from "@/components/dashboard/Customers";
-// import Customers from "@/components/dashboard/Customers";
-import Dashboard from "@/components/dashboard/Dashboard";
-import Table from "@/components/dashboard/Table";
 import SlickSlider from "@/components/login-page/SlickSlider";
 
 export default function Home() {
   return (
     <div>
       <SlickSlider />
-      <div className="overflow-hidden">
-        <div className="bg-mist-gray min-h-screen flex items-start">
-          <SideBar />
-          <div className="sm:w-11/12">
-            <Header />
-            <Dashboard />
-            {/* <Table /> */}
-          </div>{" "}
-        </div>{" "}
-      </div>{" "}
     </div>
   );
 }
