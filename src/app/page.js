63fa6270063fab9375a4671f2772cb3cd76b1da4
sachin-niyana <sachinjakhar100@gmail.com>
2024-05-29 11:53:54 +0530

    merge code

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import { MapChart } from "@/components/common/MapChart";
 import SlickSlider from "@/components/login-page/SlickSlider";
 
 export default function Home() {
   return (
     <div>
       <SlickSlider />
+      <MapChart />
     </div>
   );
 }
