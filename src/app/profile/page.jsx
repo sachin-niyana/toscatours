13d1752e32b3745dd131d67577148b0697b39b16
SunilVerma0358 <sunilverma93060@gmail.com>
2024-05-30 16:49:04 +0530

    take pull in helper

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -5,6 +5,7 @@ import { MonthList, ProfileCardList } from "@/components/common/Helper";
 import Image from "next/image";
 import { BtnArrow } from "@/components/common/Icon";
 import MapChart from "@/components/common/MapChart";
+import Typewriter from "typewriter-effect";
 
 const page = () => {
   const [Open, setOpen] = useState(false);
@@ -17,7 +18,7 @@ const page = () => {
   };
 
   return (
-    <div className="w-full mx-auto px-10 pt-6 pb-16  sm:py-10 max-h-[calc(100vh-104px)] overflow-y-scroll">
+    <div className="w-full mx-auto px-6 sm:px-10 pt-6 pb-16  sm:py-10 max-h-[calc(100vh-104px)] overflow-y-scroll">
       <div className="max-w-[1140px] mx-auto">
         <div className="flex items-center gap-5">
           <div className=" max-w-24  lg:max-w-32">
@@ -74,46 +75,71 @@ const page = () => {
               );
             })}
           </div>
-          <div className=" flex lg:block justify-center mb-14 sm:mb-0">
-            <div className="py-6 px-4 xl:px-6 hover:shadow-xs transition-all ease-linear duration-300 bg-white rounded-3xl max-w-[700px] w-full ">
-              <div className="flex justify-between items-center mb-4">
-                <p className="text-black sm:block hidden font-medium text-xl leading-normal capitalize">
-                  Verkäufe
-                </p>
-                <p className="text-black sm:hidden block font-medium text-xl leading-normal capitalize">
-                  Sales
-                </p>
-                <div className="relative inline-block">
-                  <button
-                    onClick={openDropdown}
-                    className="font-medium sm:text-base text-xs leading-normal sm:py-1 sm:py-05 py-1 sm:pr-2 pr-1 sm:pl-4 pl-3 bg-mist-gray border border-solid border-neutral-gray rounded-full text-light-black flex items-center"
-                  >
-                    This month
-                    <BtnArrow className="sm:w-10 w-7 sm:h-10 h-7" />
-                  </button>
-                  {Open && (
-                    <div className="origin-top-right absolute right-0 mt-2 w-44 rounded-lg shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-                      <ul
-                        className="py-4 px-6 bg-white gap-3 flex flex-col justify-start items-center rounded-2xl lg:h-52 sm:h-44 h-36 overflow-y-scroll"
-                        role="menu"
-                        aria-orientation="vertical"
-                        aria-labelledby="options-menu"
+          <div className="flex flex-row flex-wrap -mx-3 justify-center mb-14 sm:mb-0">
+            <div className="w-full xl:w-8/12 px-3 xl:px-4 ">
+              <div className=" flex lg:block justify-center mb-6 sm:mb-0">
+                <div className="py-6 px-4 xl:px-6 hover:shadow-xs transition-all ease-linear duration-300 bg-white rounded-3xl max-w-[700px] w-full ">
+                  <div className="flex justify-between items-center mb-4">
+                    <p className="text-black sm:block hidden font-medium text-xl leading-normal capitalize">
+                      Verkäufe
+                    </p>
+                    <p className="text-black sm:hidden block font-medium text-xl leading-normal capitalize">
+                      Sales
+                    </p>
+                    <div className="relative inline-block">
+                      <button
+                        onClick={openDropdown}
+                        className="font-medium sm:text-base text-xs leading-normal sm:py-1 sm:py-05 py-1 sm:pr-2 pr-1 sm:pl-4 pl-3 bg-mist-gray border border-solid border-neutral-gray rounded-full text-light-black flex items-center"
                       >
-                        {MonthList.map((obj, index) => (
-                          <li
-                            key={index}
-                            className="block text-black text-sm font-normal leading-normal cursor-pointer"
-                            onClick={closeDropdown}
+                        This month
+                        <BtnArrow className="sm:w-10 w-7 sm:h-10 h-7" />
+                      </button>
+                      {Open && (
+                        <div className="origin-top-right absolute right-0 mt-2 w-44 rounded-lg shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                          <ul
+                            className="py-4 px-6 bg-white gap-3 flex flex-col justify-start items-center rounded-2xl lg:h-52 sm:h-44 h-36 overflow-y-scroll"
+                            role="menu"
+                            aria-orientation="vertical"
+                            aria-labelledby="options-menu"
                           >
-                            {obj.title}
-                          </li>
-                        ))}
-                      </ul>
+                            {MonthList.map((obj, index) => (
+                              <li
+                                key={index}
+                                className="block text-black text-sm font-normal leading-normal cursor-pointer"
+                                onClick={closeDropdown}
+                              >
+                                {obj.title}
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                      )}
                     </div>
-                  )}
+                  </div>
+                  <MapChart />
                 </div>
               </div>
-              <MapChart />
+            </div>
+            <div className="w-full xl:w-4/12 px-3 xl:px-4 pt-6 xl:pt-0 ">
+              <div className="py-6 px-4 xl:px-6 hover:shadow-xs transition-all ease-linear duration-300 bg-white rounded-3xl max-w-[700px] w-full min-h-[280px] xl:h-full ">
+                <div className="text-xl font-normal text-black leading-normal text-center">
+                  <span>" Hi "</span>
+                  <Typewriter
+                    onInit={(typewriter) => {
+                      typewriter
+                        .typeString(
+                          "Stephen William Hawking was a British physicist, born on 8th January 1942. Over his entire profound career, Hawking reshaped scientific thinking of the universe, especially black holes and their illusive properties."
+                        )
+                        .start();
+                    }}
+                    options={{
+                      autoStart: true,
+                      loop: false,
+                      cursor: "",
+                    }}
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
