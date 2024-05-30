27e8d77a10a55f0042f2944eece67cf632218e71
sachin-niyana <sachinjakhar100@gmail.com>
2024-05-30 12:07:20 +0530

    take pull table

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,9 +1,12 @@
+"use client";
+import BlankDashboard from "@/components/dashboard/BlankDashboard";
 import Dashboard from "@/components/dashboard/Dashboard";
 import React from "react";
 
 const page = () => {
   return (
     <div>
+      <BlankDashboard />
       <Dashboard />
     </div>
   );
